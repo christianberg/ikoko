acbf1df7b223065198a65034395f143797bae0ed
Christian Berg <berg.christian@gmail.com>
2015-07-01 20:34:53 +0200

    Add method to multiplex posts according to audience

--- src/ikoko/core.clj
+++ src/ikoko/core.clj
@@ -1,6 +1,13 @@
 (ns ikoko.core
   (:gen-class))
 
+(defn multiplex [& entries]
+  [{:path "/public.json"
+    :filters [:json]
+    :data {:entries
+           (map #(select-keys % [:type :content :timestamp])
+                (filter #(= (:audience %) :public) entries))}}])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/ikoko/core_test.clj
+++ test/ikoko/core_test.clj
@@ -1,3 +1,15 @@
 (ns ikoko.core-test
   (:require [clojure.test :refer :all]
             [ikoko.core :refer :all]))
+
+(deftest mulitplex-test
+  (testing "Public short update"
+    (is (= (multiplex {:type :text
+                       :audience :public
+                       :content "Hello, world!"
+                       :timestamp "2015-06-30 11:37:00"})
+           [{:path "/public.json"
+             :filters [:json]
+             :data {:entries [{:timestamp "2015-06-30 11:37:00"
+                               :type :text
+                               :content "Hello, world!"}]}}]))))
